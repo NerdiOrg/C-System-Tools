539f2f8e73423343189a8e7ea802fbfca1fe66c3
William Passmore <38194792+NerdiOrg@users.noreply.github.com>
2023-10-22 14:46:42 -0400

    Delete lib/~WindowsUsers directory

--- lib/~WindowsUsers/~main.h
+++
@@ -1,5 +0,0 @@
-// FolderScanner.h
-#pragma once
-#include <vector>
-#include <filesystem>
-std::string random_aZ09(const std::string& prefix, const size_t& length, const std::string& suffix); // returns random string (using time(0)) with optional prefix/suffix
